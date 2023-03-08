09313022dbd97e6051c219fb64f63059643f5a7b
sergey.oancha <sergey.oancha@gmail.com>
2023-03-08 20:16:30 +0200

    Add auto-resize images

--- carousel.js
+++ carousel.js
@@ -1,15 +1,18 @@
 const carousel = (obj) => {
 	// Define variable
-	let startX,diff;
+	let startX;
 	let IsMouseDown,carouselOffset=0;
 	const carousel = document.querySelector('.carousel');
 	const wrapper = carousel.querySelector('.wrapper');
 	const wrapperImg = carousel.querySelector('.wrapper_img');
-	const imageWidth = wrapperImg.offsetWidth;
+	
 
 	// Init param from object
 	carousel.style.width = `calc( ${obj.width}px + (${obj.padding}px * ${obj.elements} * 2))`;
-	document.documentElement.style.setProperty("--padding-carousel-wrapper_img",obj.padding);
+	document.querySelector(':root').style.setProperty("--padding-carousel-wrapper_img",obj.padding);
+	document.querySelector(':root').style.setProperty("--width-carousel-wrapper_img-img",obj.width / obj.elements);
+	document.querySelector(':root').style.setProperty("--height-carousel-wrapper_img-img",wrapperImg.querySelector('img').height);
+	const imageWidth = wrapperImg.offsetWidth;
 
 	// Event for press mouse button
 	carousel.addEventListener('mousedown', (e) => {
@@ -27,17 +30,23 @@ const carousel = (obj) => {
 		}
 	});
 
-	// Event for unpress mouse button
-	carousel.addEventListener('mouseup', (e) => {
-		IsMouseDown = false;
+	// Method to move carousel right and left
+	var MoveCarousel = (e) => {
+		
 		const endX = (e.clientX - carousel.offsetLeft);
 		const diff = endX - startX;
 		if( diff > 0 && carouselOffset < 0) 
 			carouselOffset += carousel.offsetWidth;
-		else if( diff < 0 && carouselOffset > -(wrapper.clientWidth - (obj.elements * imageWidth))) 
+		else if( diff < 0 && carouselOffset > -(wrapper.offsetWidth - (imageWidth * obj.elements)))
 			carouselOffset -= carousel.offsetWidth;
-		
-		wrapper.style.left = carouselOffset;
-		wrapper.style.transition = "1s";
-	});
+		if (IsMouseDown) {
+			wrapper.style.left = carouselOffset;
+			wrapper.style.transition = "1s";
+			IsMouseDown = false;
+		}
+	};
+
+	// Event for unpress mouse button
+	carousel.addEventListener('mouseup', MoveCarousel, false);
+	carousel.addEventListener('mouseout', MoveCarousel, false);
 }
